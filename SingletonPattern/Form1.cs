443b763dfc63dbe47029bc11b0d0ae86aa1f0aea
Mustafa Kızılışık <mkizilisik@outlook.com>
2021-11-19 17:11:35 +0300

    Add project files.

---
+++ SingletonPattern/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace SingletonPattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, System.EventArgs e)
+        {
+            Socket client = SocketConnection.GetClient(txtIPAddress.Text.ToString(), Convert.ToInt32(txtPort.Text));
+            if (client.Connected)
+            {
+                lblConnectionStatus.Text = "Connection Started...";
+            }
+            else
+            {
+                lblConnectionStatus.Text = "An error occurred while connecting";
+            }
+        }
+    }
+}
---
+++ SingletonPattern/SocketConnection.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace SingletonPattern
+{
+    /// <summary>
+    /// Socket Connection (Singleton Pattern)
+    /// </summary>
+    class SocketConnection
+    {
+        private SocketConnection()
+        {
+        }
+
+        private static Socket client;
+
+        public static Socket GetClient(string ipAddress, int port)
+        {
+            if (client == null)
+            {
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                {
+                    ReceiveTimeout = 4500,
+                    ReceiveBufferSize = 2048,
+                    SendBufferSize = 2048
+                };
+                client.Connect(ipEndPoint);
+            }
+            return client;
+        }
+    }
+}
